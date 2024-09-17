f3d76560131d03c65bc558902851798e1ffc95a7
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-09-17 13:28:48 +0500

    add test SelenideHomeWorkTest

--- src/test/java/github/SelenideHomeWorkTest.java
+++ src/test/java/github/SelenideHomeWorkTest.java
@@ -2,8 +2,6 @@ package github;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideHomeWorkTest {
