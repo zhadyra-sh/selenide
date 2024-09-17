b21be37f65c79e348c290cf7b517f6e0a774fbde
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-09-17 13:21:49 +0500

    add test SelenideHomeWorkTest

---
+++ src/test/java/github/SelenideHomeWorkTest.java
@@ -0,0 +1,20 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideHomeWorkTest {
+    @Test
+    void openWikiTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $$("a").findBy(Condition.text("Soft assertions")).click();
+        $("div.markdown-body").shouldHave(Condition.text("JUnit5"));
+
+        sleep(5000);
+
+    }
+}
